43692eab1fc526f6fcc5773f4b671080df5e82be
Dario Küffer <dariokueffer@icloud.com>
2023-04-08 15:03:32 +0200

    added radio buttons to questions #21

--- src/components/views/Question.js
+++ src/components/views/Question.js
@@ -11,24 +11,26 @@ export default function Question() {
         "Congo River",
         "Danube"
     ]
+
+    const cssClasses = [
+        "answer-item-top-left",
+        "answer-item-top-right",
+        "answer-item-bottom-right",
+        "answer-item-bottom-left",
+    ]
     return (
         <div className="question-wrapper">
             <div className="question-item">
                 <Bubble className="bubble-button--question">{question}</Bubble>
             </div>
-            <div className="answer-item-top-left">
-                <Bubble className="bubble-button--answer">{answer[0]}</Bubble>
-            </div>
-            <div className="answer-item-top-right">
-                <Bubble className="bubble-button--answer">{answer[1]}</Bubble>
-            </div>
-            <div className="answer-item-bottom-right">
-                <Bubble className="bubble-button--answer">{answer[2]}</Bubble>
-
-            </div>
-            <div className="answer-item-bottom-left">
-                <Bubble className="bubble-button--answer">{answer[3]}</Bubble>
-            </div>
+            {answer.map((item, index) => {
+                return <div className={cssClasses[index]}>
+                    <input type="radio" id={item} name="fav_language" value={item} />
+                    <label for={item}>
+                        <Bubble className="bubble-button--answer">{item}</Bubble>
+                    </label>
+                </div>
+            })}
         </div>
     )
 }
