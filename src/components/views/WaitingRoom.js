7e42a6e6f318221b03d89c15c5c001e34addf5d9
marlen <marlenkuehn97@gmail.com>
2023-04-11 12:36:21 +0200

    added waitingroom scss and bubble

--- src/components/views/WaitingRoom.js
+++ src/components/views/WaitingRoom.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import { useHistory } from 'react-router-dom';
-import 'styles/views/Gameroom.scss';
+import 'styles/views/WaitingRoom.scss';
 import BaseContainer from "components/ui/BaseContainer";
 
 import { useLocation } from 'react-router-dom';
@@ -11,81 +11,6 @@ import RadioButtons from 'components/ui/RadioButtons';
 import Select from 'components/ui/Select';
 import BackIcon from 'components/ui/BackIcon';
 
-/*
-file is only here to test if joinRoom works with the backend
-design and further functionality is npt as it should be
-the data from the backend is received but only the question Topic can be displayed
-roomcode is also be displayed 
- */
-
-const gameMode = [
-    {
-        name: 'standard',
-        value: 'standard',
-    },
-    {
-        name: 'time tells',
-        value: 'time tells',
-    },
-    {
-        name: 'trust or bust',
-        value: 'trust or bust',
-    },
-]
-
-const questionTopic = [
-    {
-        name: 'general knowledge',
-        value: 'general knowledge',
-    },
-    {
-        name: 'art',
-        value: 'art',
-    },
-    {
-        name: 'history',
-        value: 'history',
-    },
-    {
-        name: 'food and drinks',
-        value: 'food and drinks',
-    },
-    {
-        name: 'sports',
-        value: 'sports',
-    },
-    {
-        name: 'geography',
-        value: 'geography',
-    },
-    {
-        name: 'nature',
-        value: 'nature',
-    },
-]
-
-//for the number of questions to be displayed the values from the list and the backend
-//have to have the same type -> .toString() is used at line  123 
-const numOfQuestions = [
-    {
-        name: '3',
-        value: '3',
-    },
-    {
-        name: '5',
-        value: '5',
-    },
-    {
-        name: '10',
-        value: '10'
-    },
-    {
-        name: '15',
-        value: '15',
-    },
-]
-
-
 
 
 const WaitingRoom = (props) => {
@@ -95,40 +20,34 @@ const WaitingRoom = (props) => {
 
     
     //const so that only leader can start game, does nothing usefull yet
-    const doStart = () => {
+    const startGame = () => {
     }
 
     return (
-        <BaseContainer>
-            <div className="gameroom-container">
-                <h1>Game Room <br/> {data.state.roomCode}</h1>
-                <SettingsContainer >
-                    <Select
-                        name="questionTopic"
-                        items={questionTopic}
-                        value={data.state.questionTopic}
-                        onChange={null} />
-                </SettingsContainer>
-                <SettingsContainer title="Choose a game mode:">
-                    <RadioButtons
-                        name="gamemode"
-                        items={gameMode}
-                        value={data.state.gameMode}
-                        onChange={null} />
-                </SettingsContainer>
-                <SettingsContainer title="Select the number of questions:">
-                    <RadioButtons
-                        name="number-of-questions"
-                        items={numOfQuestions}
-                        value={data.state.numOfQuestions.toString()}
-                        onChange={null} />
-                </SettingsContainer>
-
-                
-                <Bubble onClick={() => doStart()}>Get<br />Ready!</Bubble>
+        <div className="waiting-rom-wrapper">
+            <div className="bubble-item">
+            <Bubble onClick={startGame} className="bubble-button--waitingroom">Wait for players: Press to start!</Bubble>
             </div>
-            <BackIcon />
-        </BaseContainer>
+        
+            <div className="room-code">
+                room code: 
+            </div>
+
+            <div className="game-info">
+                number of questions:
+                question topic:
+                game mode:
+            </div>
+
+            <div className="player-info">
+                already joined:
+            </div>
+
+            <div className="exit-button">
+                exit
+            </div>
+
+        </div>
     );
 };
 
