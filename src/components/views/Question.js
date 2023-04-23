3907d359aa1f8c38e7bf22a85c3900bd1a09f2dd
marlen <marlenkuehn97@gmail.com>
2023-04-23 12:17:33 +0200

    adding access control error yay

--- src/components/views/Question.js
+++ src/components/views/Question.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer, useMemo } from 'react';
 import { Bubble } from 'components/ui/Bubble';
+import { useLocation, useHistory } from 'react-router-dom';
+import {getDomainSocket} from "../../helpers/getDomainSocket";
+import { format } from 'react-string-format';
+import io from "socket.io-client";
 
 import '../../styles/views/Question.scss';
 
 
-
-export default function Question() {
+const Question = props => { 
 
     //websockets need to communicate:
     // timing
@@ -17,8 +20,20 @@ export default function Question() {
     const [correctAnswer, setCorrectAnswer] = useState(null);
     const [popupValue, setPopupValue] = useState(null);
     const [radioValue, setRadioValue] = useState(null);
+    const [roomCode, setRoom] = useState("1");
+    const data = useLocation();
+  
 
-    const question = "Which river has the most capital cities on it?"
+    //url from backend for websocket connection
+    // const url = format(getDomainSocket() + "?roomCode={0}", roomCode);
+    // const socket =  io.connect(url, { transports: ['websocket'], upgrade: false, roomCode: roomCode });
+
+    const url = format(getDomainSocket() + "?roomCode={0}", roomCode);
+    const socket = useMemo(() => io.connect(url, { transports: ['websocket'], upgrade: false, roomCode: roomCode }), []);
+
+    console.log("socket acknowledged as connected:", socket.connected);
+
+    //const question = "Which river has the most capital cities on it?"
     const answer = [
         "Amazon",
         "Nile",
@@ -36,16 +51,66 @@ export default function Question() {
     const revealAnswer = () => {
         setCorrectAnswer(0);
         const timer = setTimeout(() => {
-            console.log(radioValue === answer[0]);
+            console.log('answer correct:', radioValue === answer[0]);
             setPopupValue(radioValue === answer[0]);
         }, 2000);
         return () => clearTimeout(timer);
     }
 
     const nextQuestion = () => {
+        console.log("socket acknowledged as connected with nextQuestion:", socket.connected);
+        socket.emit('start_game', {
+            message : "",
+            roomCode: roomCode,
+            type: "CLIENT"})
+          
+        // socket.emit('start_timer', {
+        //     message : "",
+        //     roomCode: roomCode,
+        //     type: "CLIENT"})
+
         setPopupValue(null);
         setCorrectAnswer(null);
     }
+    const question = null;
+
+    useEffect(async () => {
+        console.log("socket acknowledged as connected in useEffect:", socket.connected);
+
+        //everytime an event happens triggered by the socket, this function is called
+        socket.on("get_question", (data) =>{
+            console.log("question arrived:")
+            console.log(data.message)
+            question = data.message;
+        })
+
+        socket.on("get_answers", (data) =>{
+            console.log("answers arrived:")
+            console.log(data.message)
+        })
+
+        // socket.on("game_started", (incomingData) =>{
+        //     console.log("game_started received", incomingData);
+        // })
+
+        // socket.on("timer_message", (data) =>{
+        //     console.log("timer message received:")
+        //     console.log(data.message)
+        // })
+        // socket.on("timer_count", (data) =>{
+        //     console.log(data.message)
+        //     setCounter(data.message);
+        // })
+
+    }, [])
+
+    // join websocket connection again, since there was a disconnect when the push to /waitingroom happened
+    // const roomCode = data.state.roomCode
+    // const url = format(getDomainSocket() + "?roomCode={0}", roomCode);
+    // const socket = useMemo(() => io.connect(url, { transports: ['websocket'], upgrade: false, roomCode: roomCode }), []);
+
+    
+
 
     return (
         <div className="question-wrapper">
@@ -72,7 +137,6 @@ export default function Question() {
             }
 
         </div >
-
-
     )
-}
+};
+export default Question;
