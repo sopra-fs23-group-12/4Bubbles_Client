bc0637be3b2623763973ab9bdfa18e1240eb1190
Dario Küffer <dariokueffer@icloud.com>
2023-04-24 07:48:15 +0200

    added timer to start button

--- src/components/views/WaitingRoom.js
+++ src/components/views/WaitingRoom.js
@@ -1,18 +1,18 @@
 import 'styles/views/WaitingRoom.scss';
-import React, {useEffect, useMemo, useState} from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import { Bubble } from 'components/ui/Bubble';
-import {format} from "react-string-format";
+import { format } from "react-string-format";
 import io from "socket.io-client";
-import {getDomainSocket} from "../../helpers/getDomainSocket";
-import {Button} from "../ui/Button";
+import { getDomainSocket } from "../../helpers/getDomainSocket";
+import { Button } from "../ui/Button";
 
 
 
 
 // establish a websocket connection (joins namespace for only the sender client)
-const url = format(getDomainSocket() );
-const socket = io.connect(url,{transports: ['websocket'], upgrade: false});
+const url = format(getDomainSocket());
+const socket = io.connect(url, { transports: ['websocket'], upgrade: false });
 
 const WaitingRoom = (props) => {
     const history = useHistory();
@@ -25,6 +25,7 @@ const WaitingRoom = (props) => {
     const roomCode = data.state.roomCode
     const url = format(getDomainSocket() + "?roomCode={0}", roomCode);
     const socket = useMemo(() => io.connect(url, { transports: ['websocket'], upgrade: false, roomCode: roomCode }), []);
+    const [counter, setCounter] = useState(null);
 
 
 
@@ -32,14 +33,25 @@ const WaitingRoom = (props) => {
 
         // add a condition that only the leader can click this
         console.log("game started");
-        socket.emit('start_game',{
-            message : "",
+        socket.emit('start_game', {
+            message: "",
             roomCode: roomCode,
-            type: "CLIENT"})
+            type: "CLIENT"
+        })
+    }
+
+    const startTimer = () => {
+        console.log("timer started");
+        socket.emit('start_timer', {
+            message: "",
+            roomCode: roomCode,
+            type: "CLIENT"
+        })
     }
 
 
-    useEffect(async () =>{
+
+    useEffect(() => {
 
         //returns a list of members since that is the only thing in the state that changes
         socket.on("joined_players", (incomingData) => {
@@ -49,12 +61,22 @@ const WaitingRoom = (props) => {
             data.state.members = incomingData;
         })
 
-        socket.on("game_started", (incomingData) =>{
+        socket.on("game_started", (incomingData) => {
             console.log("game_started received");
             history.push(`/question`);
         })
 
-    })
+        socket.on("timer_message", (data) => {
+            console.log("timer message received:")
+            console.log(data.message)
+        })
+
+        socket.on("timer_count", (data) => {
+            console.log(data.message)
+            setCounter(data.message);
+        })
+
+    }, [])
 
     return (
         <div className="waiting-room-wrapper">
@@ -65,32 +87,39 @@ const WaitingRoom = (props) => {
                     {members.map((member) => {
                         return (
                             <div key={member.username} className="player">{member.username}</div>
-                        )})}
+                        )
+                    })}
                 </div>
             </div>
 
             <div className="bubble-item">
-                <div className = "bubble-container">
-                    <Bubble onClick={startGame} className="bubble-button--waitingroom">Wait for players: Press to start!</Bubble>
+                <div className="bubble-container">
+                    {
+                    counter === null ?
+                        <Bubble onClick={startTimer} className="bubble-button--waitingroom">Wait for players: Press to start!</Bubble>
+                        :
+                        <Bubble className="bubble-button--waitingroom">Start in {counter}s</Bubble>
+
+                    }
                 </div>
             </div>
 
             <div className="room-code">
                 room code:
-                <br/>
+                <br />
                 {data.state.roomCode}
             </div>
 
             <div className="game-info">
                 number of questions: {data.state.numOfQuestions}
-                <br/>
+                <br />
                 question topic: {data.state.questionTopic}
-                <br/>
+                <br />
                 game mode: {data.state.gameMode}
             </div>
 
             <div className="exit-button">
-                <a href = "/welcomepage"> exit </a>
+                <a href="/welcomepage"> exit </a>
             </div>
         </div>
     );
