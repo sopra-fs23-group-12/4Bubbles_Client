8cff17f7202eb5abccb96bc4601f5f57dc1203ab
Dario Küffer <dariokueffer@icloud.com>
2023-05-21 12:09:59 +0200

    changed landscape popup to have ok button

--- src/components/ui/PopUp.js
+++ src/components/ui/PopUp.js
@@ -1,15 +1,14 @@
-
-
-import React from 'react'
+import React, { useState } from 'react';
 import '../../styles/ui/pop-up-alert.scss';
 
 export default function PopUpAlert(props) {
-    return (
-        <div className="pop-up pop-up-alert">
-            <div className="pop-up__container">
-                <span>Please rotate your phone, it is better to play the game in landscape mode. 🔁</span>
-            </div>
-        </div >
-
-    )
+  const [showPopUp, setShowPopup] = useState(true);
+  return showPopUp === true ? (
+    <div className="pop-up pop-up-alert">
+      <div className="pop-up__container">
+        <span>Please rotate your phone, it is better to play the game in landscape mode. 🔁</span>
+        <button onClick={() => setShowPopup(false)} className="primary-button">ok</button>
+      </div>
+    </div>
+  ) : null;
 }
